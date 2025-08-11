ad31611eb8116d02d6b43294f93914fa431c1216
Ella B <ella55ball@gmail.com>
2025-08-11 16:00:32 +0100

    creating product model

--- src/main/java/models/Order.java
+++ src/main/java/models/Order.java
@@ -1,4 +1,60 @@
 package models;
 
-public class Order {
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+    @Entity(name = "Orders")
+    public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long basketTotal;
+
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+    @OneToMany(mappedBy = "order")
+    @JsonIgnore
+    private List<Product> productList;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getBasketTotal() {
+        return basketTotal;
+    }
+
+    public void setBasketTotal(Long basketTotal) {
+        this.basketTotal = basketTotal;
+    }
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(List<Product> productList) {
+        this.productList = productList;
+    }
+
+    public List<Integer> getQuantityList() {
+            return quantityList;
+        }
+
+     public void setQuantityList(List<Integer> quantityList) {
+            this.quantityList = quantityList;
+        }
+
+    private List<Integer> quantityList;
 }
