143b293a808f63f8fa243cf73c11726f6a8b88e3
Ella B <ella55ball@gmail.com>
2025-08-13 14:00:28 +0100

    creating integration-test branch with function to test get requests

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,3 +1,22 @@
 import "./components/nav/nav";
 import "./components/content/content";
 import "./components/shop/shop";
+
+async function getData() {
+    const url =
+        "https://nology-group-project-production.up.railway.app/api/users";
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        console.log(result);
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error(error.message);
+        }
+    }
+}
+getData();
