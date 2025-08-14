0b9fdfc551b814885bc5764b6f2b60579300f1c7
alexkneale <alexanderkneale109@gmail.com>
2025-08-14 14:23:14 +0100

    email test code

---
+++ frontend/src/components/cart/email.ts
@@ -0,0 +1,23 @@
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    auth: {
+        user: "nologygroupprojectecommerce@gmail.com", // your email
+        pass: "wgurztxwfybmdoxq", // the app password you generated, paste without spaces
+    },
+    secure: true,
+    port: 465,
+});
+(async () => {
+    await transporter.sendMail({
+        from: "nologygroupprojectecommerce@gmail.com", // your email
+        to: "alexanderkneale109@gmail.com", // the email address you want to send an email to
+        subject: "Hello", // The title or subject of the email
+        html: "Here i am", // I like sending my email as html, you can send \
+        // emails as html or as plain text
+    });
+
+    console.log("Email sent");
+})();
