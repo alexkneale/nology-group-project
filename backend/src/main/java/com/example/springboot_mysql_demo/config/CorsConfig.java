2b2cbf990e85af351a51ee9796fc7c32905b80c1
alexkneale <alexanderkneale109@gmail.com>
2025-08-14 18:25:16 +0100

    fixed root directory of SORS

--- backend/src/main/java/com/example/springboot_mysql_demo/config/CorsConfig.java
+++ backend/src/main/java/com/example/springboot_mysql_demo/config/CorsConfig.java
@@ -15,9 +15,9 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // apply to all endpoints
                         .allowedOrigins(
-                                "http://localhost:5173",   // Local Vite dev server
-                                "http://127.0.0.1:5500",
-                                "https://alexkneale.github.io/nology-group-project/"
+                                // "http://localhost:5173",   // Local Vite dev server
+                                // "http://127.0.0.1:5500",
+                                "https://alexkneale.github.io/"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
