4829f089da5af5dac49be265affa3116268a4cd0
alexkneale <alexanderkneale109@gmail.com>
2025-08-14 18:35:03 +0100

    moving between pages configuration added

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,18 @@
 import { defineConfig } from "vite";
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 export default defineConfig({
     base: "/nology-group-project/",
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                cart: resolve(__dirname, "cart.html"),
+                // add more pages here
+            },
+        },
+    },
 });
